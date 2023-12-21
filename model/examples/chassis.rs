3746f1bbdfee4eca2cec3df60a79978aa8367d2b
9506hqwy <97424592+9506hqwy@users.noreply.github.com>
2023-12-22 00:15:43 +0900

    モジュールを登録した。

---
+++ model/examples/chassis.rs
@@ -0,0 +1,37 @@
+use redfish_model::chassis::v1_23_0::Chassis;
+use redfish_model::chassis_collection::ChassisCollection;
+use redfish_model::service_root::v1_16_0::ServiceRoot;
+use reqwest::blocking::ClientBuilder;
+use std::env::args;
+use std::error::Error;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let url = args()
+        .nth(1)
+        .ok_or("Specify URL `https://127.0.0.1:443`.")?;
+
+    let client = ClientBuilder::new()
+        .danger_accept_invalid_certs(true)
+        .build()?;
+
+    let json = client.get(format!("{url}/redfish/v1")).send()?.text()?;
+    let root = serde_json::from_str::<ServiceRoot>(&json)?;
+
+    if let Some(idref) = &root.chassis {
+        if let Some(uri) = &idref.odata_id {
+            let json = client.get(format!("{url}{uri}")).send()?.text()?;
+            let collections = serde_json::from_str::<ChassisCollection>(&json)?;
+
+            for member in collections.members {
+                if let Some(uri) = &member.odata_id {
+                    let json = client.get(format!("{url}{uri}")).send()?.text()?;
+                    let chassis = serde_json::from_str::<Chassis>(&json)?;
+
+                    dbg!(&chassis);
+                }
+            }
+        }
+    }
+
+    Ok(())
+}
---
+++ model/examples/root.rs
@@ -0,0 +1,21 @@
+use redfish_model::service_root::v1_16_0::ServiceRoot;
+use reqwest::blocking::ClientBuilder;
+use std::env::args;
+use std::error::Error;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let url = args()
+        .nth(1)
+        .ok_or("Specify URL `https://127.0.0.1:443`.")?;
+
+    let client = ClientBuilder::new()
+        .danger_accept_invalid_certs(true)
+        .build()?;
+
+    let json = client.get(format!("{url}/redfish/v1")).send()?.text()?;
+    let root = serde_json::from_str::<ServiceRoot>(&json)?;
+
+    dbg!(&root);
+
+    Ok(())
+}
